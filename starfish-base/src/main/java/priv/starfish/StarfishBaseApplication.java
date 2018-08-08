47dd3ba30d863dcf507967ae453f32d5cf123ecb
jiahaixin <jiahaixin@360.cn>
2018-08-08 16:25:54 +0800

    create progect

---
+++ starfish-base/src/main/java/priv/starfish/StarfishBaseApplication.java
@@ -0,0 +1,12 @@
+package priv.starfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarfishBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarfishBaseApplication.class, args);
+    }
+}
---
+++ starfish-fang/src/main/java/priv/starfish/CloudAppApplication.java
@@ -0,0 +1,12 @@
+package priv.starfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAppApplication.class, args);
+    }
+}
