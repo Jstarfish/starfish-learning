7f1d5b7a463e00fe7c3d84af1e89d0ac9fbde977
jiahaixin <jiahaixin@360.cn>
2019-09-24 11:14:46 +0800

    :newspaper:  结构修改

---
+++ starfish-learn/src/main/java/priv/starfish/constructorOrderTest/App.java
@@ -0,0 +1,36 @@
+package priv.starfish.constructorOrderTest;
+
+/**
+ * @author: jiahaixin
+ * @date: 2019/9/24 10:42
+ * @description:
+ */
+public class App {
+    public static void main(String[] args) {
+        App2 app2=new App2();
+    }
+
+    int a =110;//Foo的成员变量
+    public App() {
+        System.out.println("我是父类的构造方法");
+        add();
+    }
+    public void add() {
+        System.out.println("调用我了吗？");
+        a++;
+        System.out.println(a);
+    }
+}
+class App2 extends App{
+    int a =100;
+    public App2() {
+        System.out.println(a);
+    }
+    @Override
+    public void add() {
+        System.out.println("重写了父类的add方法");
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+    }
+}
